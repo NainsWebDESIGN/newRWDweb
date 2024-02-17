5ac68d1080c44de4a1b3b8afdefdde6eb6137239
Nains <jason20226@gmail.com>
2024-02-17 18:04:09 +0800

    202402171804

--- deliciousNainsWeb/src/app/app.component.ts
+++ deliciousNainsWeb/src/app/app.component.ts
@@ -1,108 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor() {
-    const preloader = document.querySelector("[data-preaload]");
+export class AppComponent implements OnInit {
+  @ViewChild("preaload") preaload: ElementRef;
+  @ViewChild("header") header: ElementRef;
+  @ViewChild("navbar") navbar: ElementRef;
+  @ViewChild("overlay") overlay: ElementRef;
+  @ViewChild("heroSlider") heroSlider: ElementRef;
+  @ViewChild("parallaxItem") parallaxItem: ElementRef;
+  lastScrollPos: number = 0;
+  sliderItem;
+  lastActiveSliderItem;
+  currentSliderPos: number = 0;
+  autoSliderInterval;
+  constructor() { }
+  ngOnInit() {
+    this.sliderItem = this.heroSlider.nativeElement.children;
+    this.lastActiveSliderItem = this.sliderItem[0];
+  }
+  @HostListener('window:load', ['$event'])
+  onPageLoad() {
+    this.preaload.nativeElement.classList.add("loaded");
+    document.body.classList.add("loaded");
+    this.sliderInterval();
+  }
+  @HostListener('window:scroll', ['$event'])
+  onPageScroll() {
+    let top = window.scrollY;
 
-    // window.addEventListener("load", () => {
-    //   preloader.classList.add("loaded");
-    //   document.body.classList.add("loaded");
-    // });
-
-    const eventElement = (el, event, back) => {
-      for (let i = 0, len = el.length; i < len; i++) {
-        el[i].addEventListener(event, back);
-      }
-    },
-      navbar = document.querySelector("[data-navbar"),
-      // navTogglers = document.querySelectorAll("[data-nav-toggler]"),
-      overlay = document.querySelector("[data-overlay]");
-
-    function toggleNavbar() {
-      navbar.classList.toggle("active");
-      overlay.classList.toggle("active");
-      document.body.classList.toggle("nav-active");
+    this.header.nativeElement.classList[top >= 50 ? "add" : "remove"]("active");
+    if (top >= 50) {
+      this.header.nativeElement.classList[this.lastScrollPos < top ? "add" : "remove"]("hide");
+      this.lastScrollPos = top;
     }
 
-    const header = document.querySelector("[data-header]");
-
-    let lastScrollPos = 0;
-
-    const hideHeader = function () {
-      const isScrollBottom = lastScrollPos < window.scrollY;
-      header.classList[isScrollBottom ? "add" : "remove"]("hide");
-      lastScrollPos = window.scrollY;
-    };
-
-    window.addEventListener("scroll", () => {
-      header.classList[window.scrollY >= 50 ? "add" : "remove"]("active");
-      if (window.scrollY >= 50) {
-        hideHeader();
-      }
-    });
-
-    const heroSlider = document.querySelector("[data-hero-slider]"),
-      heroSliderItems = document.querySelectorAll("[data-hero-slider-item]"),
-      heroSliderPrevBtn = document.querySelector("[data-prev-btn]"),
-      heroSliderNextBtn = document.querySelector("[data-next-btn]");
-
-    let currentSliderPos = 0,
-      lastActiveSliderItem = heroSliderItems[0];
-
-    const updateSliderPos = () => {
-      lastActiveSliderItem.classList.remove("active");
-      heroSliderItems[currentSliderPos].classList.add("active");
-      lastActiveSliderItem = heroSliderItems[currentSliderPos];
-    }
-      // ,
-      //   sliderNext = () => {
-      //     currentSliderPos =
-      //       currentSliderPos >= heroSliderItems.length - 1 ? 0 : currentSliderPos + 1;
-      //     updateSliderPos();
-      //   },
-      //   sliderPrev = () => {
-      //     currentSliderPos =
-      //       currentSliderPos <= 0 ? heroSliderItems.length - 1 : currentSliderPos - 1;
-      //     updateSliderPos();
-      //   }
-      ;
-
-    // heroSliderNextBtn.addEventListener("click", sliderNext());
-    // heroSliderPrevBtn.addEventListener("click", sliderPrev());
-
-    // let autoSliderInterval = setInterval(() => sliderNext(), 7000);
-    // const autoSlide = () => {
-    //   autoSliderInterval = setInterval(() => sliderNext(), 7000);
-    // };
-    // addEventListener([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", () => {
-    //   clearInterval(autoSliderInterval);
-    // });
-    // addEventListener(
-    //   [heroSliderNextBtn, heroSliderPrevBtn],
-    //   "mouseout",
-    //   autoSlide()
-    // );
-
-    // window.addEventListener("load", autoSlide());
-
-    const parallaxItems = document.querySelectorAll("[data-parallax-item]");
+  }
+  @HostListener('mousemove', ['$event'])
+  handleMousemove(event) {
+    let children = this.parallaxItem.nativeElement.children,
+      parallax = [children[0], children[1]];
     let x, y;
-    window.addEventListener("mousemove", (event) => {
-      x = (event.clientX / window.innerWidth) * 10 - 5;
-      y = (event.clientY / window.innerHeight) * 10 - 5;
-      x = x - x * 2;
-      y = y - y * 2;
-      // for (let i = 0, len = parallaxItems.length; i < len; i++) {
-      //   x = x * Number(parallaxItems[i].dataset.parallaxSpeed);
-      //   y = y * Number(parallaxItems[i].dataset.parallaxSpeed);
-      //   parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
-      // }
-    });
-
+    x = (event.clientX / window.innerWidth) * 10 - 5;
+    y = (event.clientY / window.innerHeight) * 10 - 5;
+    x = x - x * 2;
+    y = y - y * 2;
+    for (let i = 0, len = parallax.length; i < len; i++) {
+      x = x * Number(parallax[i].dataset.parallaxSpeed);
+      y = y * Number(parallax[i].dataset.parallaxSpeed);
+      parallax[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
+    }
+  }
+  sliderInterval() {
+    clearInterval(this.autoSliderInterval);
+    this.autoSliderInterval = setInterval(() => this.sliderMove(1), 7000);
+  }
+  toggleNavbar() {
+    this.navbar.nativeElement.classList.toggle("active");
+    this.overlay.nativeElement.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+  }
+  updateSliderPos() {
+    this.lastActiveSliderItem.classList.remove("active");
+    this.sliderItem[this.currentSliderPos].classList.add("active");
+    this.lastActiveSliderItem = this.sliderItem[this.currentSliderPos];
+  }
+  sliderMove(move: number) {
+    switch (move) {
+      case 1:
+        this.currentSliderPos =
+          this.currentSliderPos >= this.sliderItem.length - 1 ? 0 : this.currentSliderPos + 1;
+        break;
+      case -1:
+        this.currentSliderPos =
+          this.currentSliderPos <= 0 ? this.sliderItem.length - 1 : this.currentSliderPos - 1;
+        break;
+    }
+    this.updateSliderPos();
   }
 }
