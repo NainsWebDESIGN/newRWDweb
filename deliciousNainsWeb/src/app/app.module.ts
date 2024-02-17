afd8c5764f38dc9edfdaab6b9a823a14d07aa07d
Nains <jason20226@gmail.com>
2024-02-17 15:44:02 +0800

    2024-02-17 15:43

--- deliciousNainsWeb/src/app/app.module.ts
+++ deliciousNainsWeb/src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-
+// Component
 import { AppComponent } from './app.component';
+
+// Service
+// import { xxx } from '@service';
+
+// Pipe && Component
+// import { xxx } from '@pipeModule';
+// import { xxx } from '@comModule';
 
 
 @NgModule({
@@ -10,7 +19,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
